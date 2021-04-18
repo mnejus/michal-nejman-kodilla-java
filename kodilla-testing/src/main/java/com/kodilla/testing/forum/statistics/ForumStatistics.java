6f9ded3900f409d41883fe219cd9194985a68c34
mnejus <nejman.michal@gmail.com>
2021-04-18 15:03:57 +0200

    6.7 - added forum statistics

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -1,2 +1,51 @@
-package com.kodilla.testing.forum.statistics;public class ForumStatistics {
+package com.kodilla.testing.forum.statistics;
+
+public class ForumStatistics {
+
+    private Statistics statistics;
+
+    int usersNum;
+    int postsNum;
+    int commentsNum;
+    double avgUserPosts;
+    double avgUserComments;
+    double avgPostComments;
+
+    public ForumStatistics(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public double getAvgUserPosts() {
+        avgUserPosts = statistics.postCount() / (float) statistics.userNames().size();
+        return avgUserPosts;
+    }
+
+    public double getAvgUserComments() {
+        avgUserComments = statistics.commentsCount() / (float) statistics.userNames().size();
+        return avgUserComments;
+    }
+
+    public double getAvgPostComments() {
+        avgPostComments = statistics.commentsCount() / (float) statistics.postCount();
+        return avgPostComments;
+
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        usersNum = statistics.userNames().size();
+        postsNum = statistics.postCount();
+        commentsNum = statistics.commentsCount();
+        avgUserPosts = getAvgUserPosts();
+        avgUserComments = getAvgUserComments();
+        avgPostComments = getAvgPostComments();
+    }
+
+    public void showStatistics() {
+        System.out.println("Number of users: " + usersNum);
+        System.out.println("Number of posts: " + postsNum);
+        System.out.println("Number of comments: " + commentsNum);
+        System.out.println("Average number of posts per user: " + avgUserPosts);
+        System.out.println("Average number of comments per user: " + avgUserComments);
+        System.out.println("Average number of comments per post: " + avgPostComments);
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -1,4 +1,118 @@
 package com.kodilla.testing.forum.statistics;
 
+import com.kodilla.testing.library.LibraryDatabase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class ForumStatisticsTestSuite {
+
+    private static int testCounter = 0;
+
+    @BeforeEach
+    public void before() {
+        testCounter ++;
+        System.out.println("Preparing to execute test # " + testCounter);
+    }
+
+    private List<String> generateListOfUsers(int usersQuantity) {
+        List<String> usersList = new ArrayList<>();
+        for(int n = 0; n < usersQuantity; n++) {
+            usersList.add("user" + n);
+        }
+        return usersList;
+    }
+
+    @Mock
+    private Statistics statisticsMock;
+
+    @Test
+    void testCalculateAdvStatistics0Posts0Comments0Users() {
+        //Given
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+        List<String> userList = generateListOfUsers(0);
+        when(statisticsMock.userNames()).thenReturn(userList);
+        when(statisticsMock.postCount()).thenReturn(0);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+
+        //When
+        double resultAvgUserPosts = forumStatistics.getAvgUserPosts();
+        double resultAvgUserComments = forumStatistics.getAvgUserComments();
+        double resultAvgPostComments = forumStatistics.getAvgPostComments();
+
+        //Then
+        assertNotEquals(0.0, resultAvgUserPosts);
+        assertNotEquals(0.0, resultAvgPostComments);
+        assertNotEquals(0.0, resultAvgPostComments);
+    }
+
+    @Test
+    void testCalculateAdvStatistics1000Posts0Comments100Users() {
+        //Given
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+        List<String> userList = generateListOfUsers(100);
+        when(statisticsMock.userNames()).thenReturn(userList);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+        when(statisticsMock.postCount()).thenReturn(1000);
+
+        //When
+        double resultAvgUserPosts = forumStatistics.getAvgUserPosts();
+        double resultAvgPostComments = forumStatistics.getAvgPostComments();
+        double resultAvgUserComments = forumStatistics.getAvgUserComments();
+
+        //Then
+        assertEquals(10.0, resultAvgUserPosts);
+        assertEquals(0.0, resultAvgPostComments);
+        assertEquals(0.0, resultAvgUserComments);
+    }
+
+    @Test
+    void testCalculateAdvStatistics1000PostsLessComments100Users() {
+        //Given
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+        List<String> userList = generateListOfUsers(100);
+        when(statisticsMock.userNames()).thenReturn(userList);
+        when(statisticsMock.commentsCount()).thenReturn(500);
+        when(statisticsMock.postCount()).thenReturn(1000);
+
+        //When
+        double resultAvgUserPosts = forumStatistics.getAvgUserPosts();
+        double resultAvgPostComments = forumStatistics.getAvgPostComments();
+        double resultAvgUserComments = forumStatistics.getAvgUserComments();
+
+        //Then
+        assertEquals(10.0, resultAvgUserPosts);
+        assertEquals(0.5, resultAvgPostComments);
+        assertEquals(5, resultAvgUserComments);
+    }
+
+    @Test
+    void testCalculateAdvStatistics1000PostsMoreComments100Users() {
+        //Given
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+        List<String> userList = generateListOfUsers(100);
+        when(statisticsMock.userNames()).thenReturn(userList);
+        when(statisticsMock.commentsCount()).thenReturn(10000);
+        when(statisticsMock.postCount()).thenReturn(1000);
+
+        //When
+        double resultAvgUserPosts = forumStatistics.getAvgUserPosts();
+        double resultAvgPostComments = forumStatistics.getAvgPostComments();
+        double resultAvgUserComments = forumStatistics.getAvgUserComments();
+
+        //Then
+        assertEquals(10.0, resultAvgUserPosts);
+        assertEquals(10.0, resultAvgPostComments);
+        assertEquals(100, resultAvgUserComments);
+    }
 }
