b8fa141da09566ca2a194dcc94d00655172fe60d
mnejus <nejman.michal@gmail.com>
2021-06-04 21:30:52 +0200

    Revert "m-pong"
    This reverts commit a75c12c5a298430501686607cdfb66928f3a55e1.

--- kodilla-first-project/src/main/java/com/kodilla/pong/PaddleAi.java
+++ kodilla-first-project/src/main/java/com/kodilla/pong/PaddleAi.java
@@ -1,22 +1,2 @@
-package com.kodilla.pong;
-
-public abstract class PaddleAi {
-
-    private final Paddle paddle;
-    private final Game game;
-
-    protected PaddleAi(Paddle paddle, Game game) {
-        this.paddle = paddle;
-        this.game = game;
-    }
-
-    public Game getGame() {
-        return game;
-    }
-
-    public Paddle getPaddle() {
-        return paddle;
-    }
-
-    public abstract void update(double deltaTime);
+package com.kodilla.pong;public class PaddleAi {
 }
