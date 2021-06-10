03b31447bbbcdc25f885923efb7bbbbf6ef3767a
mnejus <62847127+mnejus@users.noreply.github.com>
2021-06-10 23:17:42 +0200

    Delete kodilla-first-project directory

--- kodilla-first-project/src/main/java/com/kodilla/pong/Utilities.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.pong;
-
-public class Utilities {
-    private Utilities() {}
-    public static double degreesToRadians(double degrees) {
-        return degrees * Math.PI / 180;
-    }
-}
