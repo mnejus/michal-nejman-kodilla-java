cf66b9bd6e2ebbc0701facc8133f6db9bfd22e5b
mnejus <nejman.michal@gmail.com>
2021-06-12 09:14:43 +0200

    14.1

---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.spring.intro.shape;
+
+import org.junit.jupiter.api.Test;
+
+public class DrawerTestSuite {
+    @Test
+    public void testDoDrawing() {
+        //Given
+        Drawer drawer = new Drawer();
+
+        //When
+        drawer.doDrawing();
+
+        //Then
+        //do  nothing
+    }
+}
